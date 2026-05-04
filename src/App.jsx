964619deb3d959f97a8f34737787ec4c6f23204a
Abdulaziz1214 <yormatov3@gmail.com>
2026-05-04 09:34:42 +0500

    header dobavil

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import Header from "./components/Header"
 
 const App = () => {
   return (
     <div>
       <h1 className='text-red-400 text-center bg-amber-300'>Hello Gulchapchap</h1>
+      <Header/>
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  return (
+    <div>
+      <div className="box w-55 h-55 bg-blue-500">
+        hello Doni
+      </div>
+    </div>
+  )
+}
+
+export default Header
